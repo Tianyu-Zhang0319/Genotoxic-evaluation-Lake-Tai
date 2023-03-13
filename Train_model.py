0fd65e9db2de7e2f9bdb7fc9c8f25803c2bfe997
TZ <t.zhang@nki.nl>
2023-03-13 20:29:37 +0100

    Create Train_model.py

---
+++ Train_model.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import os
+from IPython.display import Image, display
+from tensorflow.keras.preprocessing.image import load_img
+from PIL import ImageOps
+from tensorflow import keras
+import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras import layers
+import random
+import cv2
+
+input_dir = "./SegCell_20230313/images/"
+target_dir = "./SegCell_20230313/trimaps/"
+img_size = (512, 512)
+num_classes = 3
+batch_size = 32
+
+input_img_paths = sorted(
+    [
+        os.path.join(input_dir, fname)
+        for fname in os.listdir(input_dir)
+        if fname.endswith(".png")
+    ]
+)
+target_img_paths = sorted(
+    [
+        os.path.join(target_dir, fname)
+        for fname in os.listdir(target_dir)
+        if fname.endswith(".png") and not fname.startswith(".")
+    ]
+)
+
+
+
+class Feifei_SegCell(keras.utils.Sequence):
+    """Helper to iterate over the data (as Numpy arrays)."""
+
+    def __init__(self, batch_size, img_size, input_img_paths, target_img_paths):
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.input_img_paths = input_img_paths
+        self.target_img_paths = target_img_paths
+
+    def __len__(self):
+        return len(self.target_img_paths) // self.batch_size
+
+    def __getitem__(self, idx):
+        """Returns tuple (input, target) correspond to batch #idx."""
+        i = idx * self.batch_size
+        batch_input_img_paths = self.input_img_paths[i : i + self.batch_size]
+        batch_target_img_paths = self.target_img_paths[i : i + self.batch_size]
+        x = np.zeros((self.batch_size,) + self.img_size + (3,), dtype="float32")
+        for j, path in enumerate(batch_input_img_paths):
+            img = load_img(path, target_size=self.img_size)
+            img=img
+            x[j] = img
+        y = np.zeros((self.batch_size,) + self.img_size + (1,), dtype="uint8")
+        for j, path in enumerate(batch_target_img_paths):
+            img = load_img(path, target_size=self.img_size, color_mode="grayscale")
+            y[j] = np.expand_dims(img, 2)
+        return x, y
+
+
+
+
+def get_model(img_size, num_classes):
+    inputs = keras.Input(shape=img_size + (3,))
+
+    ### [First half of the network: downsampling inputs] ###
+
+    # Entry block
+    x = layers.Conv2D(32, 3, strides=2, padding="same")(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
+
+    previous_block_activation = x  # Set aside residual
+
+    # Blocks 1, 2, 3 are identical apart from the feature depth.
+    for filters in [64, 128, 256, 512]:
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.SeparableConv2D(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.MaxPooling2D(3, strides=2, padding="same")(x)
+
+        # Project residual
+        residual = layers.Conv2D(filters, 1, strides=2, padding="same")(
+            previous_block_activation
+        )
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    ### [Second half of the network: upsampling inputs] ###
+
+    for filters in [512, 256, 128, 64, 32]:
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.Activation("relu")(x)
+        x = layers.Conv2DTranspose(filters, 3, padding="same")(x)
+        x = layers.BatchNormalization()(x)
+
+        x = layers.UpSampling2D(2)(x)
+
+        # Project residual
+        residual = layers.UpSampling2D(2)(previous_block_activation)
+        residual = layers.Conv2D(filters, 1, padding="same")(residual)
+        x = layers.add([x, residual])  # Add back residual
+        previous_block_activation = x  # Set aside next residual
+
+    # Add a per-pixel classification layer
+    outputs = layers.Conv2D(num_classes, 3, activation="softmax", padding="same")(x)
+
+    # Define the model
+    model = keras.Model(inputs, outputs)
+    return model
+
+
+# Free up RAM in case the model definition cells were run multiple times
+keras.backend.clear_session()
+
+# Build model
+model = get_model(img_size, num_classes)
+#model.summary()
+
+
+
+# Split our img paths into a training and a validation set
+train_samples = 249
+random.Random(1203).shuffle(input_img_paths)
+random.Random(1203).shuffle(target_img_paths)
+train_input_img_paths = input_img_paths[:train_samples]
+train_target_img_paths = target_img_paths[:train_samples]
+val_input_img_paths = input_img_paths[train_samples:]
+val_target_img_paths = target_img_paths[train_samples:]
+
+# Instantiate data Sequences for each split
+train_gen = Feifei_SegCell(
+    batch_size, img_size, train_input_img_paths, train_target_img_paths
+)
+val_gen = Feifei_SegCell(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+
+# Configure the model for training.
+# We use the "sparse" version of categorical_crossentropy
+# because our target data is integers.
+weights = [1, 1, 10000]
+model.compile(optimizer="Adam", loss="sparse_categorical_crossentropy", loss_weights=weights, metrics=['accuracy'])
+
+callbacks = [
+    keras.callbacks.ModelCheckpoint("./results/Cell_Segmentation.h5", monitor='val_loss', mode='min', save_best_only=True)
+]
+
+# Train the model, doing validation at the end of each epoch.
+epochs = 200
+model.fit(train_gen, epochs=epochs, validation_data=val_gen, callbacks=callbacks)
+
+### Test
+from tensorflow.keras.models import load_model
+model = load_model('./results/Cell_Segmentation.h5')
+
+val_gen = Feifei_SegCell(batch_size, img_size, val_input_img_paths, val_target_img_paths)
+val_preds = model.predict(val_gen)
